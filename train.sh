95bc625292b588a6a6dddadc105c2de7cd0bc11c
KysonYang001 <143758329+KysonYang001@users.noreply.github.com>
2025-03-11 20:06:47 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 python train_degrade.py --config configs/degradation.yaml  --savedir checkpoint/degradation 
-python train_SR.py --config configs/train_INR.yaml --savedir checkpoint/INR
+python train_INR.py --config configs/train_INR.yaml --savedir checkpoint/INR
