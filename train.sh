ea94357b4472c201a6f605bf12d9c8dde5a7fd4c
Jibin Yang <143758329+KysonYang001@users.noreply.github.com>
2025-08-03 16:04:06 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-python train_degrade.py --config configs/degradation.yaml  --savedir checkpoint/degradation 
-python train_INR.py --config configs/train_INR.yaml --savedir checkpoint/INR
+python train_degrade.py --config configs/degradation.yaml  --savepath checkpoint/degradation 
+python train_INR.py --config configs/train_INR-rdn-liif-volume.yaml --savedir checkpoint/INR
