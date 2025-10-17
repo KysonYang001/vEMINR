26ab69a127207dcf1619cc2bc7d0ac5465a93d81
Jibin Yang <143758329+KysonYang001@users.noreply.github.com>
2025-10-18 02:44:48 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-python test.py  --model_weight (path to model weight.pth) --save (path to save yz reconstructed volume) --test_config (path to test_yz.yaml) &
-python test.py  --model_weight (path to model weight.pth) --save (path to save xz reconstructed volume) --test_config (path to test_xz.yaml)
-
-python mean.py --yz (path to yz reconstructed volume) --xz (path to xz reconstructed volume) --save (path to save final reconstructed volume)
