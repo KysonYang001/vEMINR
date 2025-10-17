4c607277674bb70a41a031bf3925b7d5bb9af088
Jibin Yang <143758329+KysonYang001@users.noreply.github.com>
2025-10-18 02:44:55 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,2 +0,0 @@
-python train_degrade.py --config configs/degradation.yaml  --savepath checkpoint/degradation 
-python train_INR.py --config configs/train_INR-rdn-liif-volume.yaml --savedir checkpoint/INR
